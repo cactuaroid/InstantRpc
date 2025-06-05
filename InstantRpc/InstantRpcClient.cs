c3bea2428f0053781f4ab602128849e1d258a5f3
cactuaroid <solamiredoredore@gmail.com>
2025-06-06 00:08:15 +0900

    feat: add InstantRpc proj

---
+++ InstantRpc/InstantRpcClient.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Xml.Linq;
+
+namespace InstantRpc
+{
+    public class InstantRpcClient<T>
+    {
+        public string InstanceId { get; }
+
+        public InstantRpcClient(string instanceId = "")
+        {
+            InstanceId = instanceId;
+        }
+
+        public void Set<TValue>(Expression<Func<T, TValue>> expression, TValue value) where TValue : struct
+        {
+            Set(expression, () => value);
+        }
+
+        public void Set<TValue>(Expression<Func<T, TValue>> expression, Expression<Func<TValue>> value)
+        {
+            var memberName = GetMemberAndMethodPath(expression);
+            var arg = SerializeArgument(value.Body);
+
+            Execute("SET", memberName, arg);
+        }
+
+        public TValue Get<TValue>(Expression<Func<T, TValue>> expression)
+        {
+            if (!CanParse(typeof(TValue))) { throw new ArgumentException($"{typeof(TValue)} does not support Parse."); }
+
+            var memberName = GetMemberAndMethodPath(expression);
+            var response = Execute("GET", memberName);
+
+            return Parse<TValue>(response);
+        }
+
+        public TReturn Invoke<TReturn>(Expression<Func<T, TReturn>> expression)
+        {
+            if (!CanParse(typeof(TReturn))) { throw new ArgumentException($"{typeof(TReturn)} does not support Parse."); }
+
+            var response = InvokeImpl(expression);
+            return Parse<TReturn>(response);
+        }
+
+        public void Invoke(Expression<Action<T>> expression)
+        {
+            InvokeImpl(expression);
+        }
+
+        private string InvokeImpl(LambdaExpression expression)
+        {
+            var parameters = new List<string>();
+
+            if (expression.Body is MethodCallExpression method)
+            {
+                var methodName = GetMemberAndMethodPath(expression);
+                var args = new XElement("args", SerializeArguments(method.Arguments));
+
+                return Execute("INVOKE", methodName, args);
+            }
+            else
+            {
+                throw new ArgumentException("expression must be method call");
+            }
+        }
+
+        private string Execute(string operation, string memberName, XElement args = null)
+        {
+            return ExecuteImpl($"{operation}|{typeof(T).AssemblyQualifiedName}|{InstanceId}|{memberName}|{args?.ToString() ?? ""}");
+        }
+
+        private string ExecuteImpl(string message)
+        {
+            using (var pipeClient = new NamedPipeClientStream("InstantRpcPipe"))
+            {
+                pipeClient.Connect();
+
+                var ss = new StreamString(pipeClient);
+                ss.WriteString(message);
+
+                var response = ss.ReadString();
+
+                var elements = response.Split('|');
+                var success = bool.Parse(elements[0]);
+                var parameter = elements[1]; // returning value or error message
+
+                if (!success) { throw new InvalidOperationException("Operation failed. Detail: " + parameter); }
+
+                return parameter;
+            }
+        }
+
+        private static XElement SerializeArgument(Expression arg)
+        {
+            return SerializeArguments(new[] { arg }).Single();
+        }
+
+        private static IEnumerable<XElement> SerializeArguments(IReadOnlyCollection<Expression> args)
+        {
+            // ex. ) new MyClass1(new MyClass2("a"), "b") { A = "c", B = "d" }
+            // ->
+            // <ctor type="MyClass1">
+            //    <ctor type="MyClass2">
+            //        <value type="System.String">a</value>
+            //    </ctor>
+            //    <value type="System.String">b</value>
+            //    <init type="System.String" prop="A">c</init>
+            //    <init type="System.String" prop="B">d</init>
+            // </ctor>
+
+            return args.Select((x) =>
+            {
+                if (x is NewExpression @new)
+                {
+                    return 
+                        new XElement("ctor",
+                            new XAttribute("type", @new.Type.AssemblyQualifiedName),
+                            SerializeArguments(@new.Arguments));
+                }
+                else if (x is MemberInitExpression memberInit)
+                {
+                    return
+                        new XElement("ctor",
+                            new XAttribute("type", memberInit.NewExpression.Type.AssemblyQualifiedName),
+                            SerializeArguments(memberInit.NewExpression.Arguments),
+                            memberInit.Bindings.OfType<MemberAssignment>().Select((m) =>
+                                new XElement("init",
+                                    new XAttribute("type", (m.Member as PropertyInfo).PropertyType.AssemblyQualifiedName),
+                                    new XAttribute("prop", m.Member.Name),
+                                    EvaluateExpression(m.Expression))));
+                }
+                else
+                {
+                    return
+                        new XElement("value",
+                            new XAttribute("type", x.Type.AssemblyQualifiedName),
+                            EvaluateExpression(x));
+                }
+            });
+        }
+
+        private static string GetMemberName<TValue>(Expression<Func<T, TValue>> expression)
+            => (expression.Body is MemberExpression member) ? member.Member.Name : throw new NotSupportedException();
+
+        public static string GetMemberAndMethodPath(LambdaExpression expression)
+        {
+            var pathBuilder = new StringBuilder();
+            Expression currentExpression = expression.Body;
+
+            while (currentExpression != null)
+            {
+                if (currentExpression is MemberExpression memberExpression)
+                {
+                    // property call
+                    if (pathBuilder.Length > 0)
+                    {
+                        pathBuilder.Insert(0, ".");
+                    }
+                    pathBuilder.Insert(0, memberExpression.Member.Name);
+                    currentExpression = memberExpression.Expression;
+                }
+                else if (currentExpression is MethodCallExpression methodCallExpression)
+                {
+                    // method call
+                    if (pathBuilder.Length > 0)
+                    {
+                        pathBuilder.Insert(0, ".");
+                    }
+                    pathBuilder.Insert(0, methodCallExpression.Method.Name);
+                    currentExpression = methodCallExpression.Object;
+                }
+                else if (currentExpression is ParameterExpression)
+                {
+                    // reached to parameter of lambda, goal!
+                    break;
+                }
+                else if (currentExpression is UnaryExpression unaryExpression && unaryExpression.NodeType == ExpressionType.Convert)
+                {
+                    // cast
+                    currentExpression = unaryExpression.Operand;
+                }
+                else
+                {
+                    throw new NotSupportedException($"Unsupported expression type: {currentExpression.NodeType}");
+                }
+            }
+
+            return pathBuilder.ToString();
+        }
+
+        public static object EvaluateExpression(Expression expression)
+        {
+            if (expression is ConstantExpression constant)
+            {
+                // constant
+                var value = constant.Value;
+
+                if (!CanParse(value.GetType())) { throw new ArgumentException($"{value.GetType()} does not support Parse."); }
+
+                return value;
+            }
+            else
+            {
+                // variable requires invoke to evaluate
+                var lambda = Expression.Lambda<Func<object>>(Expression.Convert(expression, typeof(object)));
+                var value = lambda.Compile().Invoke();
+
+                if (!CanParse(value.GetType())) { throw new ArgumentException($"{value.GetType()} does not support Parse."); }
+
+                return value;
+            }
+        }
+
+        private static bool CanParse(Type type)
+        {
+            if (type == typeof(string)) { return true; }
+            if (type.IsEnum) { return true; }
+
+            var parseMethod = type.GetMethod("Parse", new Type[] { typeof(string) });
+            return parseMethod != null;            
+        }
+
+        private static TValue Parse<TValue>(string value)
+        {
+            if (typeof(TValue) == typeof(string)) { return (TValue)(object)value; }
+            if (typeof(TValue).IsEnum) { return (TValue)Enum.Parse(typeof(TValue), value); }
+
+            var parseMethod = typeof(TValue).GetMethod("Parse", new Type[] { typeof(string) });
+            if (parseMethod is null) { throw new NotSupportedException($"'Parse(string)' is not implemented on type [{typeof(TValue)}]."); }
+
+            return (TValue)parseMethod.Invoke(null, new[] { value });
+        }
+    }
+}
---
+++ InstantRpc/InstantRpcService.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace InstantRpc
+{
+    public static class InstantRpcService
+    {
+        public static IReadOnlyDictionary<(string TypeName, string Key), object> Targets => _targets;
+        private static Dictionary<(string TypeName, string Key), object> _targets = new Dictionary<(string, string), object>();
+        private static Task _thread;
+
+        // static class is not supported yet
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="target"></param>
+        /// <param name="instanceId"></param>
+        /// <exception cref="ArgumentException"></exception>
+        public static void Expose<T>(T target, string instanceId = "")
+        {
+            var key = (typeof(T).AssemblyQualifiedName, instanceId);
+            if (_targets.ContainsKey(key))
+            {
+                throw new ArgumentException(nameof(instanceId), $"Duplicated instanceId: {key}");
+            }
+
+            _targets[key] = target;
+
+            if (_thread is null)
+            {
+                _thread = Task.Run(() => ServerThread());
+            }
+        }
+
+        private static void ServerThread()
+        {
+            while (true)
+            {
+                using (var pipeServer = new NamedPipeServerStream("InstantRpcPipe"))
+                {
+                    pipeServer.WaitForConnection();
+                    string response;
+
+                    var ss = new StreamString(pipeServer);
+
+                    var received = ss.ReadString();
+
+                    {
+                        // command string format
+                        // 　command name|target type|target instance ID|parameters
+                        // ex.
+                        // 　"GET|MyApp.MainWindow||Top|"
+                        // 　"SET|MyApp.MainWindow||Top|<value type="System.Double">10</value>"
+                        // 　"INVOKE|MyApp.MainWindow||Do|<value type="System.String">test1</value><value type="System.String">test2</value>"
+
+                        var elements = received.Split('|');
+                        var command = elements[0];
+                        var type = elements[1];
+                        var key = elements[2];
+                        var member = elements[3];
+                        var args = elements[4];
+
+                        if (!Targets.ContainsKey((type, key)))
+                        {
+                            response = $"{false}|{(type, key)} is not exposed.";
+                        }
+                        else
+                        {
+                            var target = Targets[(type, key)];
+
+                            try
+                            {
+                                switch (command)
+                                {
+                                    case "GET":
+                                        response = Get(target, member);
+                                        break;
+                                    case "SET":
+                                        response = Set(target, member, args);
+                                        break;
+                                    case "INVOKE":
+                                        response = Invoke(target, member, args);
+                                        break;
+                                    default:
+                                        throw new NotSupportedException();
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                response = $"{false}|{ex}";
+                            }
+                        }
+
+                        var write = ss.WriteString(response);
+
+                        pipeServer.WaitForPipeDrain();
+                    }
+                }
+            }
+        }
+
+        private static string Get(object target, string memberPath)
+        {
+            var (instance, memberName) = ExtractPath(target, memberPath);
+            var prop = instance.GetType().GetProperty(memberName);
+            var result = prop.GetValue(instance);
+
+            return $"{true}|{result}";
+        }
+
+        private static string Set(object target, string memberPath, string arg)
+        {
+            var (instance, memberName) = ExtractPath(target, memberPath);
+            var prop = instance.GetType().GetProperty(memberName);
+            var argXml = XElement.Parse(arg);
+
+            var param = DeserializeArgument(argXml);
+            prop.SetValue(instance, param);
+
+            return $"{true}|";
+        }
+
+        private static string Invoke(object target, string memberPath, string args)
+        {
+            var (instance, memberName) = ExtractPath(target, memberPath);
+            var argsXml = XElement.Parse(args);
+            var param = DeserializeArguments(argsXml.Elements());
+
+            var method = instance.GetType().GetMethod(memberName, argsXml.Elements().Select((x) => GetType(x)).ToArray());
+            var result = method.Invoke(instance, param);
+
+            return $"{true}|{result}";
+        }
+
+        private static (object Instance, string MemberName) ExtractPath(object target, string memberPath)
+        {
+            var members = memberPath.Split('.');
+            var currentInstance = target;
+            for (var i = 0; i < members.Length - 1; i++)
+            {
+                currentInstance = currentInstance.GetType().GetProperty(members[i]).GetValue(currentInstance);
+            }
+
+            return (currentInstance, members.Last());
+        }
+
+        private static object DeserializeArgument(XElement element)
+        {
+            return DeserializeArguments(new[] { element }).Single();
+        }
+
+        private static object[] DeserializeArguments(IEnumerable<XElement> elements)
+        {
+            return elements.Select((x) =>
+            {
+                var type = GetType(x);
+                if (x.Name == "ctor")
+                {
+                    // call constructor
+                    var argsXml = x.Elements().Where((e) => e.Name == "ctor" || e.Name == "value");
+                    var args = DeserializeArguments(argsXml);
+                    var instance = Activator.CreateInstance(type, args);
+
+                    // initialize properties
+                    foreach (var init in x.Elements("init"))
+                    {
+                        type
+                        .GetProperty(init.Attribute("prop").Value)
+                        .SetValue(instance, Parse(init));
+                    }
+
+                    return instance;
+                }
+                else // value
+                {
+                    return Parse(x);
+                }
+            }).ToArray();
+        }
+
+        private static Type GetType(XElement element)
+        {
+            return Type.GetType(element.Attribute("type").Value);
+        }
+
+        private static object Parse(XElement element)
+        {
+            return Parse(GetType(element), element.Value);
+        }
+
+        private static object Parse(Type type, string value)
+        {
+            if (type == typeof(string)) { return value; }
+            if (type.IsEnum) { return Enum.Parse(type, value); }
+
+            var parseMethod = type.GetMethod("Parse", new Type[] { typeof(string) });
+            if (parseMethod is null) { throw new NotSupportedException($"'Parse(string)' is not implemented on type [{type}]."); }
+
+            return parseMethod.Invoke(null, new[] { value });
+        }
+    }
+}
