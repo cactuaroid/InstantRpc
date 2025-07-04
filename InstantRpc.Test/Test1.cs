3454bbf13c2c3d4754175f79c14a48184904ba72
cactuaroid <solamiredoredore@gmail.com>
2025-07-04 23:29:17 +0900

    test: refactor: simplified

--- InstantRpc.Test/Test1.cs
+++ InstantRpc.Test/Test1.cs
@@ -32,61 +32,80 @@ namespace InstantRpc.Test
         public void GetSet_Primitive()
         {
             _client.Set((x) => x.Top, 10);
-            Assert.AreEqual(10, _client.Get((x) => x.Top));
+            var v = _client.Get((x) => x.Top);
+            Assert.AreEqual(10, v);
         }
 
         [TestMethod]
         public void GetSet_Enum()
         {
             _client.Set((x) => x.Visibility, Visibility.Collapsed);
-            Assert.AreEqual(Visibility.Collapsed, _client.Get((x) => x.Visibility));
+
+            var v1 = _client.Get((x) => x.Visibility);
+            Assert.AreEqual(Visibility.Collapsed, v1);
+
             _client.Set((x) => x.Visibility, Visibility.Visible);
-            Assert.AreEqual(Visibility.Visible, _client.Get((x) => x.Visibility));
+
+            var v2 = _client.Get((x) => x.Visibility);
+            Assert.AreEqual(Visibility.Visible, v2);
         }
 
         [TestMethod]
         public void Invoke()
         {
             _client.Invoke((x) => x.Hide());
-            Assert.IsFalse(_client.Get((x) => x.IsVisible));
+
+            var v1 = _client.Get((x) => x.IsVisible);
+            Assert.IsFalse(v1);
+
             _client.Invoke((x) => x.Show());
-            Assert.IsTrue(_client.Get((x) => x.IsVisible));
+
+            var v2 = _client.Get((x) => x.IsVisible);
+            Assert.IsTrue(v2);
         }
 
         [TestMethod]
         public void GetSetInvoke_PropertyChain()
         {
             _client.Set((x) => ((MainWindowViewModel)x.DataContext).Value, "changed");
-            Assert.AreEqual("changed", _client.Get((x) => ((MainWindowViewModel)x.DataContext).Value));
-            Assert.AreEqual("changed", _client.Invoke((x) => ((MainWindowViewModel)x.DataContext).GetValue()));
+            var v1 = _client.Get((x) => ((MainWindowViewModel)x.DataContext).Value);
+            var v2 = _client.Invoke((x) => ((MainWindowViewModel)x.DataContext).GetValue());
+            Assert.AreEqual("changed", v1);
+            Assert.AreEqual("changed", v2);
         }
 
         [TestMethod]
         public void Invoke_PropertyChain_PrimitiveArgs()
         {
-            Assert.AreEqual(3, _client.Invoke((x) => ((MainWindowViewModel)x.DataContext).Add(1, 2)));
+            var v = _client.Invoke((x) => ((MainWindowViewModel)x.DataContext).Add(1, 2));
+            Assert.AreEqual(3, v);
         }
 
         [TestMethod]
         public void Invoke_PropertyChain_ConstructorArgs()
         {
-            Assert.AreEqual("123", _client.Invoke((x) => ((MainWindowViewModel)x.DataContext).Concat(new MyParam("1", "2"), new MyParam() { Value = "3" })));
+            var v = _client.Invoke((x) => ((MainWindowViewModel)x.DataContext).Concat(new MyParam("1", "2"), new MyParam() { Value = "3" }));
+            Assert.AreEqual("123", v);
         }
 
         [TestMethod]
         public void GetSetInvoke_Tuple()
         {
             _client.Set((x) => ((MainWindowViewModel)x.DataContext).Tuple, (3, 4));
-            Assert.AreEqual((3, 4), _client.Get((x) => ((MainWindowViewModel)x.DataContext).Tuple));
-            Assert.AreEqual((3, 4), _client.Invoke((x) => ((MainWindowViewModel)x.DataContext).GetTuple()));
+            var v1 = _client.Get((x) => ((MainWindowViewModel)x.DataContext).Tuple);
+            var v2 = _client.Invoke((x) => ((MainWindowViewModel)x.DataContext).GetTuple());
+            Assert.AreEqual((3, 4), v1);
+            Assert.AreEqual((3, 4), v2);
         }
 
         [TestMethod]
         public void GetSetInvoke_ParsableClass()
         {
             _client.Set((x) => ((MainWindowViewModel)x.DataContext).ParsableValue, new MyParam("1", "2"));
-            Assert.AreEqual("12", _client.Get((x) => ((MainWindowViewModel)x.DataContext).ParsableValue).Value);
-            Assert.AreEqual("12", _client.Invoke((x) => ((MainWindowViewModel)x.DataContext).GetParsableValue()).Value);
+            var v1 = _client.Get((x) => ((MainWindowViewModel)x.DataContext).ParsableValue);
+            var v2 = _client.Invoke((x) => ((MainWindowViewModel)x.DataContext).GetParsableValue());
+            Assert.AreEqual("12", v1.Value);
+            Assert.AreEqual("12", v2.Value);
         }
     }
 }
